ff17153814a546793b697a1feedba2a0f016cbd7
AisunMagaramova <maharramovaaysun1122@gamil.com>
2026-02-07 12:02:03 +0000

    t1

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -1,13 +1,24 @@
-#!/usr/bin/python3
-import sys
+#!/usr/bin/node
+/*
+    Print a square with the character #
 
-if len(sys.argv) <= 1:
-    sys.stderr.write("Missing argument\n")
-    sys.stderr.write("Usage: ./1-print_square.py <size>\n")
-    sys.stderr.write("Example: ./1-print_square.py 8\n")
-    sys.exit(1)
+    The size of the square must be the first argument
+    of the program.
+*/
 
-size = int(sys.argv[1])
 
-for i in range(size):
-    print("#" * size)
+if (process.argv.length <= 2) {
+    process.stderr.write("Missing argument\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.exit(1)
+}
+
+size = parseInt(process.argv[2], 10)
+
+for (let i = 0 ; i < size ; i++) {
+    for (let j = 0 ; j < size ; j++) {
+        process.stdout.write("#");
+    }
+    process.stdout.write("\n");
+}
